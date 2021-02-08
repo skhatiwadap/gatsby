5c7ee412ce2db4be4ed754518233fa7250f27021
skhatiwada01 <skhatiwada@panth.com>
2021-02-08 11:54:02 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
       resolve: `gatsby-source-drupal`,
       options: {
         baseUrl: `http://d1140566ac04.ngrok.io/poc-pantheon-drupal/web/`,
+        secret: 'hello',
         apiBase: `jsonapi`,
         basicAuth: {
           username: `admin`,
