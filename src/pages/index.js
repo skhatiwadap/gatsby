e38a3434457076a0ed824de6f96b0f7f5ef3cd7f
skhatiwada01 <skhatiwada@panth.com>
2021-02-10 12:36:31 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = ({data}) => {
       {
         data.allNodeHomePageContent.edges.map((item, i) => (
           <>
-            <h3>{item.node.title}</h3>
+            <h3>{item.node.title} - My Update</h3>
             <div style={{display: "flex", "flex-direction": (i % 2 === 0 ? "row-reverse": "row")}}> 
               <div dangerouslySetInnerHTML={{__html: item.node.body.value}}></div>
               <img style={{width:'20%', margin: 10}} src={item.node.relationships.field_side_image.localFile.url} alt='' />
